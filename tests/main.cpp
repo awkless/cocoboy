42e22ae6f50129d24ffb1757ac3094876db55c99
Jason Pena <jasonpena@awkless.com>
2025-05-20 13:40:07 -0600

    feat: Setup skeleton of test suite
    Forgot to do this beforehand. Now the CI/CD workflow for quality
    checking should pass.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+#include <gtest/gtest.h>
+
+int
+main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
