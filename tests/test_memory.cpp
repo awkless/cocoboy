77f6470869aca078218db41b4c73c6e4adfdea5f
Jason Pena <jasonpena@awkless.com>
2025-06-12 12:41:40 -0600

    test(memory): Remove lint comments
    These lint comments to shut up clang-tidy are no longer needed because I
    disabled the `readability-magic-number` feature flag.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- tests/test_memory.cpp
+++ tests/test_memory.cpp
@@ -13,10 +13,10 @@ TEST_CASE("RegisterBit& RegisterBit::operator=(T val)", "[register_bit]")
     cocoboy::soc::RegisterBit<1, 3, uint8_t> bit2(reg);
     cocoboy::soc::RegisterBit<4, 4, uint8_t> bit3(reg);
 
-    bit1 = 0x1; // NOLINT
-    bit2 = 0x2; // NOLINT
-    bit3 = 0xF; // NOLINT
-    REQUIRE(reg == 0b11110101); // NOLINT
+    bit1 = 0x1;
+    bit2 = 0x2;
+    bit3 = 0xF;
+    REQUIRE(reg == 0b11110101);
 }
 
 TEST_CASE("RegisterBit::operator T() const", "[register_bit]")
@@ -27,15 +27,15 @@ TEST_CASE("RegisterBit::operator T() const", "[register_bit]")
     cocoboy::soc::RegisterBit<4, 4, uint8_t> bit3(reg);
 
 
-    bit1 = 0x1; // NOLINT
-    bit2 = 0x2; // NOLINT
-    bit3 = 0xF; // NOLINT
+    bit1 = 0x1;
+    bit2 = 0x2;
+    bit3 = 0xF;
     uint8_t val1 = bit1;
     uint8_t val2 = bit2;
     uint8_t val3 = bit3;
-    REQUIRE(val1 == 0x1); // NOLINT
-    REQUIRE(val2 == 0x2); // NOLINT
-    REQUIRE(val3 == 0xF); // NOLINT
+    REQUIRE(val1 == 0x1);
+    REQUIRE(val2 == 0x2);
+    REQUIRE(val3 == 0xF);
 }
 
 TEST_CASE("RegisterPair& RegisterPair::operator=(T val)", "[register_pair]")
@@ -44,14 +44,14 @@ TEST_CASE("RegisterPair& RegisterPair::operator=(T val)", "[register_pair]")
     cocoboy::soc::Register<uint8_t> reg2(0x00);
     cocoboy::soc::RegisterPair<uint16_t, uint8_t> reg_pair(reg1, reg2);
 
-    reg_pair = 0xDEAD; // NOLINT
-    REQUIRE(reg_pair == 0xDEAD); // NOLINT
-    REQUIRE(reg1 == 0xDE); // NOLINT
-    REQUIRE(reg2 == 0xAD); // NOLINT
+    reg_pair = 0xDEAD;
+    REQUIRE(reg_pair == 0xDEAD);
+    REQUIRE(reg1 == 0xDE);
+    REQUIRE(reg2 == 0xAD);
 
-    reg1 = 0x12; // NOLINT
-    reg2 = 0x34; // NOLINT
-    REQUIRE(reg_pair == 0x1234); // NOLINT
+    reg1 = 0x12;
+    reg2 = 0x34;
+    REQUIRE(reg_pair == 0x1234);
 }
 
 TEST_CASE("RegisterPair& RegisterPair::operator P() const", "[register_pair]")
@@ -60,10 +60,10 @@ TEST_CASE("RegisterPair& RegisterPair::operator P() const", "[register_pair]")
     cocoboy::soc::Register<uint8_t> reg2(0x00);
     cocoboy::soc::RegisterPair<uint16_t, uint8_t> reg_pair(reg1, reg2);
 
-    reg1 = 0x12; // NOLINT
-    reg2 = 0x34; // NOLINT
+    reg1 = 0x12;
+    reg2 = 0x34;
     uint16_t val = reg_pair;
-    REQUIRE(val == 0x1234); // NOLINT
+    REQUIRE(val == 0x1234);
 }
 
 TEST_CASE("RegisterPair& RegisterPair::operator++()", "[register_pair]")
