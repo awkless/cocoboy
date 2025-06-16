71be9a067136b1119d9d7f6eba24d7ae9e8c2385
Jason Pena <jasonpena@awkless.com>
2025-06-15 19:04:02 -0600

    refactor: Use jump table approach for instruction decoding
    I separated out the hardware state of the SM83 CPU implementation into
    its own type. I am kind of going for a hybrid data/object oriented
    approach for my implementation. Normally for a DOD approach you would
    work with raw data. However, I encapsulated register logic to make sure
    that I would not goof up bit manipulation logic for various opcode
    implementations. I think I still need some auxiliary functions to handle
    the carry and half carry flags, but I will worry about that once I begin
    messing with ALU opcode implementations.
    
    For now, I am going to share `Sm83State` across the various opcode
    implementations so they can be called through a jump table that returns
    an `Opcode` type that houses what I believe will be needed for the
    `Sm83` type to execute opcode instructions. This special jump table is
    just called `opcode_jump_table` which is constructed through a constexpr
    function named `new_opcode_jump_table`.
    
    Why build a constexpr jump table through a constexpr function? Because
    it is my best alternative to array designators for C++. I understand why
    array designators are not allowed in C++, but it still sucks that I
    can't use them. Thus, the approach shown in the code is my best way to
    get array designators in ISO C++. Is this slightly over engineered?
    Maybe.
    
    Finally, I added a `step` function as a way for `Sm83` to execute a
    single opcode. The plan is to run this through a loop at some point in
    the future. Plus, I added a few accessor methods that will mainly be
    used either for timing or testing purposes.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/cbgb/cpu.cpp
+++ src/cbgb/cpu.cpp
@@ -3,13 +3,104 @@
 
 #include <cstdint>
 #include <memory>
+#include <string>
 
-#include "cbgb/cpu.hpp"
-
+#include <fmt/format.h>
 #include <spdlog/logger.h>
 
+#include "cbgb/cpu.hpp"
 #include "cbgb/memory.hpp"
 
 namespace cbgb {
-// TODO: Write stuff here!
+enum OpcodeKind : uint8_t {
+    LD_B_B = 0x40,
+};
+
+void ld_b_b(Sm83State& cpu)
+{
+    cpu.b = cpu.b;
+}
+
+struct Opcode final {
+    std::string_view mnemonic;
+    unsigned int length;
+    unsigned int mcycle;
+    void (*execute)(Sm83State&);
+};
+
+constexpr std::array<Opcode, 256> new_opcode_jump_table()
+{
+    std::array<Opcode, 256> table = {};
+    table[OpcodeKind::LD_B_B] = Opcode { "LD B, B", 1, 1, ld_b_b };
+    return table;
+}
+constexpr std::array<Opcode, 256> opcode_jump_table = new_opcode_jump_table();
+
+UndefinedOpcode::UndefinedOpcode(std::string what)
+    : m_what(what)
+{
+}
+
+const char* UndefinedOpcode::what() const noexcept
+{
+    return m_what.c_str();
+}
+
+Sm83State::Sm83State(MemoryBus& bus)
+    : pc(0x0100)
+    , sp(0xFFFE)
+    , af(a, f)
+    , bc(b, c)
+    , de(d, e)
+    , hl(h, l)
+    , a(0x01)
+    , f(0xB0)
+    , b(0x00)
+    , c(0x13)
+    , d(0x00)
+    , e(0xD8)
+    , h(0x01)
+    , l(0x4D)
+    , fz(f)
+    , fn(f)
+    , fh(f)
+    , fc(f)
+    , memory(bus)
+{
+}
+
+Sm83::Sm83(std::shared_ptr<spdlog::logger> log, MemoryBus& bus)
+    : m_state(bus)
+    , m_mcycles(0)
+    , m_logger(log)
+{
+    m_logger->trace("Construct new SM83 CPU");
+}
+
+void Sm83::step()
+{
+    uint8_t target = m_state.memory.read(m_state.pc++);
+    Opcode opcode = opcode_jump_table[target];
+    if (opcode.execute == nullptr) {
+        throw UndefinedOpcode(fmt::format(
+            "Undefined opcode: [{0:04X}: {1:02X}] {2}", m_state.pc - 1, target, opcode.mnemonic
+        ));
+    }
+
+    m_logger->debug(
+        "Execute [{0:04X}: {1:02X}]: {2}", m_state.pc - opcode.length, target, opcode.mnemonic
+    );
+    opcode.execute(m_state);
+    m_mcycles += opcode.mcycle;
+}
+
+const Sm83State& Sm83::get_state()
+{
+    return m_state;
+}
+
+unsigned int Sm83::get_mcycle_count()
+{
+    return m_mcycles;
+}
 } // namespace cbgb
--- src/cbgb/cpu.hpp
+++ src/cbgb/cpu.hpp
@@ -6,15 +6,61 @@
 
 #include <array>
 #include <cstdint>
+#include <exception>
 #include <limits>
 #include <memory>
+#include <string_view>
 
 #include <spdlog/logger.h>
 
 #include "cbgb/memory.hpp"
 
 namespace cbgb {
-// TODO: Write cpu
+class UndefinedOpcode final : public std::exception {
+public:
+    UndefinedOpcode(std::string what);
+
+    const char* what() const noexcept;
+
+private:
+    std::string m_what;
+};
+
+struct Sm83State {
+    Register<uint16_t> pc;
+    Register<uint16_t> sp;
+    RegisterPair<uint16_t, uint8_t> af;
+    RegisterPair<uint16_t, uint8_t> bc;
+    RegisterPair<uint16_t, uint8_t> de;
+    RegisterPair<uint16_t, uint8_t> hl;
+    Register<uint8_t> a;
+    Register<uint8_t> f;
+    Register<uint8_t> b;
+    Register<uint8_t> c;
+    Register<uint8_t> d;
+    Register<uint8_t> e;
+    Register<uint8_t> h;
+    Register<uint8_t> l;
+    RegisterBitField<7, 1, uint8_t> fz;
+    RegisterBitField<6, 1, uint8_t> fn;
+    RegisterBitField<5, 1, uint8_t> fh;
+    RegisterBitField<4, 1, uint8_t> fc;
+    MemoryBus& memory;
+    Sm83State(MemoryBus& bus);
+};
+
+class Sm83 final {
+public:
+    Sm83(std::shared_ptr<spdlog::logger> log, MemoryBus& bus);
+    void step();
+    const Sm83State& get_state();
+    unsigned int get_mcycle_count();
+
+private:
+    Sm83State m_state;
+    unsigned int m_mcycles;
+    std::shared_ptr<spdlog::logger> m_logger;
+};
 } // namespace cbgb
 
 #endif // CBGB_CPU_HPP
